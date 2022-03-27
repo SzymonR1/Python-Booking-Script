ae07deb8a5c530e17e6ae65dd6341a5356e2e379
Szymon <szymonrenka34@gmail.com>
2022-03-27 22:59:29 +0100

    Files print correctly and booking ID is working
    Files print correctly now, and the booking ID is now working, and matched to file name.

--- booking.py
+++ booking.py
@@ -1,9 +1,9 @@
-import sys # might remove - still testing
-import os
+import sys, os
+os.chdir('Programming-Project')
 
 i = 0
 
-while os.path.exists(f"file_{i}.txt"):
+while os.path.exists(f'booking_{i}.txt'):
     i += 1
 
 
@@ -18,10 +18,10 @@ def menu():
         review_bookings()
 
     if user_input == 3:
-        sys.exit(0) # imported - still testing
+        sys.exit(0)
 
     else:
-        menu() # testing to see if working correctly
+        menu()
 
 
 def make_a_booking():
@@ -33,7 +33,7 @@ def make_a_booking():
     print('LONG ISLAND HOLIDAYS - Making a Booking\n========================================')
     family_name = str(input('Enter your family name: '))
     phone_number = int(input('Enter your phone number: '))
-    accommodation_type = int(input('Choose your accommodation type:\n1. Deluxe Caravan\n2. Standard Caravan\n3. Camp Site\n4. No Booking\nChoose an option: '))
+    accommodation_type = int(input('Choose your accommodation type:\n1. Deluxe Caravan (€2000)\n2. Standard Caravan (€1600)\n3. Camp Site (€200)\n4. No Booking\nChoose an option: '))
 
     if accommodation_type == 1:
         accommodation_type_name = 'Deluxe'
@@ -48,7 +48,7 @@ def make_a_booking():
         accommodation_cost = 200
 
     if accommodation_type == 4:
-        sys.exit(0) # imported - still testing
+        sys.exit(0)
 
     group_size = int(input('How many people in your group?: '))
     family_pool_pass = int(input('Do you require a family pool pass?:\n1. Yes\n2. No\nChoose an option: '))
@@ -63,31 +63,23 @@ def make_a_booking():
 
     kids_amount = int(input('How many kids will join the kids club?: '))
 
-    print('Booking Details\n===============', file=open(f"booking_{i}.txt", "a"))
+    print('Booking Details\n===============', file=open(f'booking_{i}.txt', 'a'))
 
-    print(f'Surname: {family_name.capitalize()}', file=open(f"booking_{i}.txt", "a"))
-    print('Booking ID: ', file=open(f"booking_{i}.txt", "a")) # booking id - not yet implemented - come back later
-    print(f'Accommodation Type: {accommodation_type_name}', file=open(f"booking_{i}.txt", "a"))
-    print(f'No. of People: {group_size}', file=open(f"booking_{i}.txt", "a"))
-    print(f'Pool Pass: {family_pool_pass_name}', file=open(f"booking_{i}.txt", "a"))
-    print(f'No. for kids club: {kids_amount}', file=open(f"booking_{i}.txt", "a"))
-    print(f'Accommodation cost: {accommodation_cost}', file=open(f"booking_{i}.txt", "a"))
+    print(f'Surname: {family_name.capitalize()}', file=open(f'booking_{i}.txt', 'a'))
+    print(f'Booking ID: {i}', file=open(f'booking_{i}.txt', 'a')) # booking id - not yet implemented - come back later
+    print(f'Accommodation Type: {accommodation_type_name}', file=open(f'booking_{i}.txt', 'a'))
+    print(f'No. of People: {group_size}', file=open(f'booking_{i}.txt', 'a'))
+    print(f'Pool Pass: {family_pool_pass_name}', file=open(f'booking_{i}.txt', 'a'))
+    print(f'Kids joining kids club: {kids_amount}', file=open(f'booking_{i}.txt', 'a'))
+    print(f'Accommodation cost: {accommodation_cost}', file=open(f'booking_{i}.txt', 'a'))
+    print(f'Phone number: {phone_number}', file=open(f'booking_{i}.txt', 'a'))
 
     total_cost = accommodation_cost + (kids_amount * 100) + family_pool_pass_cost
 
-    print(f'Total cost: {total_cost}', file=open(f"booking_{i}.txt", "a"))
+    print(f'Total cost: {total_cost}', file=open(f'booking_{i}.txt', 'a'))
 
 
 def review_bookings():
-    print('LONG ISLAND HOLIDAYS - Review Bookings\n======================================')
-    print(f'Deluxe Caravan: ')
-    print(f'Standard Caravan: ')
-    print(f'Camp Site: \n')
-    print(f'Total no. of pool passes: ')
-    print(f'No. for kids club: ')
-    print(f'Most popular accommodation: ')
-    print(f'Expected income: ')
-    print(f'Average per booking: ')
-    print(f'Number of remaining sites: ')
+    print('temporary') # work on this - need to implement something similar to Q3 from Week 7 to get this to work
 
 menu()
