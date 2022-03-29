457d43403a450d002bf6792c13eeb8b8883e3816
Szymon <szymonrenka34@gmail.com>
2022-03-29 12:47:34 +0100

    Files named correctly and input loops
    Files are now named correctly and two of the inputs are now looped.

--- booking.py
+++ booking.py
@@ -1,15 +1,14 @@
-import sys, os
+import sys
+import os
 os.chdir('Programming-Project')
 
 i = 0
 
-while os.path.exists(f'booking_{i}.txt'):
-    i += 1
-
 
 def menu():
     print('LONG ISLAND HOLIDAYS\n=====================')
-    user_input = int(input('1. Make a booking\n2. Review Bookings\n3. Exit\nChoose an option: '))
+    user_input = int(
+        input('1. Make a booking\n2. Review Bookings\n3. Exit\nChoose an option: '))
 
     if user_input == 1:
         make_a_booking()
@@ -29,11 +28,30 @@ def make_a_booking():
     accommodation_cost = 0
 
     family_pool_pass_cost = 0
-    
+
+    # TODO: - need to append output into text files
+
     print('LONG ISLAND HOLIDAYS - Making a Booking\n========================================')
-    family_name = str(input('Enter your family name: '))
-    phone_number = int(input('Enter your phone number: '))
-    accommodation_type = int(input('Choose your accommodation type:\n1. Deluxe Caravan (€2000)\n2. Standard Caravan (€1600)\n3. Camp Site (€200)\n4. No Booking\nChoose an option: '))
+    while True:
+        surname = str(input('Enter your surname: '))
+        if len(surname) >= 15 or len(surname) <= 0:
+            print('Enter a valid surname (1-15) characters long.')
+            continue
+        else:
+            break
+
+    while True:
+        phone_number = str(input('Enter your phone number: '))
+        if len(phone_number) >= 12 or phone_number.isupper() or phone_number.islower():
+            print('Phone number is too long, or it contains a letter.')
+            continue
+        else:
+            break
+
+
+# TODO: LOOP THE REMAINING FUNCTIONS
+    accommodation_type = int(input(
+        'Choose your accommodation type:\n1. Deluxe Caravan (€2000)\n2. Standard Caravan (€1600)\n3. Camp Site (€200)\n4. No Booking\nChoose an option: '))
 
     if accommodation_type == 1:
         accommodation_type_name = 'Deluxe'
@@ -51,35 +69,31 @@ def make_a_booking():
         sys.exit(0)
 
     group_size = int(input('How many people in your group?: '))
-    family_pool_pass = int(input('Do you require a family pool pass?:\n1. Yes\n2. No\nChoose an option: '))
+    family_pool_pass = int(
+        input('Do you require a family pool pass?:\n1. Yes\n2. No\nChoose an option: '))
 
     if family_pool_pass == 1:
         family_pool_pass_cost = 150
         family_pool_pass_name = 'Yes'
-    
+
     if family_pool_pass == 2:
         family_pool_pass_cost = 0
         family_pool_pass_name = 'No'
 
     kids_amount = int(input('How many kids will join the kids club?: '))
 
-    print('Booking Details\n===============', file=open(f'booking_{i}.txt', 'a'))
+    total_cost = accommodation_cost + \
+        (kids_amount * 100) + family_pool_pass_cost
 
-    print(f'Surname: {family_name.capitalize()}', file=open(f'booking_{i}.txt', 'a'))
-    print(f'Booking ID: {i}', file=open(f'booking_{i}.txt', 'a')) # booking id - not yet implemented - come back later
-    print(f'Accommodation Type: {accommodation_type_name}', file=open(f'booking_{i}.txt', 'a'))
-    print(f'No. of People: {group_size}', file=open(f'booking_{i}.txt', 'a'))
-    print(f'Pool Pass: {family_pool_pass_name}', file=open(f'booking_{i}.txt', 'a'))
-    print(f'Kids joining kids club: {kids_amount}', file=open(f'booking_{i}.txt', 'a'))
-    print(f'Accommodation cost: {accommodation_cost}', file=open(f'booking_{i}.txt', 'a'))
-    print(f'Phone number: {phone_number}', file=open(f'booking_{i}.txt', 'a'))
+    file_name = surname + '_'
 
-    total_cost = accommodation_cost + (kids_amount * 100) + family_pool_pass_cost
-
-    print(f'Total cost: {total_cost}', file=open(f'booking_{i}.txt', 'a'))
+    print(f'Booking Details\n===============\nSurname: {surname.capitalize()}\nBooking ID: {i}\nAccommodation Type: {accommodation_type_name}\nNo. of People: {group_size}\nPool Pass: {family_pool_pass_name}\nKids joining kids club: {kids_amount}\nPhone number: {phone_number}\nAccommodation cost: {accommodation_cost} EUR\nTotal cost: {total_cost} EUR', file=open(
+        f'{file_name+str(i)}.txt', 'a'))
 
 
 def review_bookings():
-    print('temporary') # work on this - need to implement something similar to Q3 from Week 7 to get this to work
+    # FIXME: - need to implement something similar to Q3 from Week 7 to get this to work
+    input('Test')
+
 
 menu()
